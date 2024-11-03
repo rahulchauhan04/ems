514e7b343f367da736dfb9502cb4b1e1bd52a1ab
rahulchauhan04 <rahulg0004@gmail.com>
2024-11-03 20:41:28 +0530

    Employee dashboard created

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../other/Header";
 import TaskListsNumbers from "../other/TaskListsNumbers";
+import TaskLilst from "../TaskList/TaskList";
 
 const EmployeeDashboard = () => {
   return (
      <div className="p-10 bg-[#1c1c1c] h-screen">
           <Header />
           <TaskListsNumbers />
+          <TaskLilst />
      </div>
   );
 };
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const TaskLilst = () => {
+  return (
+     <div id="tasklist" className="overflow-x-auto h-[55%] flex items-center justify-start gap-10 flex-nowrap w-full py-5 mt-10">
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">3 Nov 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make EMS Project</h2>
+        <p className="text-sm mt-2">Complete this Project today, you have to do it Today, because You gotta lot to do My Boy.</p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">3 Nov 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make EMS Project</h2>
+        <p className="text-sm mt-2">Complete this Project today, you have to do it Today, because You gotta lot to do My Boy.</p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-pink-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">3 Nov 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make EMS Project</h2>
+        <p className="text-sm mt-2">Complete this Project today, you have to do it Today, because You gotta lot to do My Boy.</p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-purple-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">3 Nov 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make EMS Project</h2>
+        <p className="text-sm mt-2">Complete this Project today, you have to do it Today, because You gotta lot to do My Boy.</p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">3 Nov 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make EMS Project</h2>
+        <p className="text-sm mt-2">Complete this Project today, you have to do it Today, because You gotta lot to do My Boy.</p>
+      </div>
+     </div>
+  )
+};
+
+export default TaskLilst;
